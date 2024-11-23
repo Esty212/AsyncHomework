206a3b4e8e1b28f2b35bbc778c02f4defa447b70
Esty212 <esty.dziubov@gmail.com>
2024-11-23 14:36:56 +0200

    async code

--- AsyncProj/Assets/Scenes/NewBehaviourScript.cs
+++ AsyncProj/Assets/Scenes/NewBehaviourScript.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        
+        //task 1
+        Debug.Log("I'm hungry");
+        CookMeals();
+        Debug.Log("going to order food");
+
+        //task 2
+        Debug.Log("Went to the resataurant");
+        await ItalianFood();
+        Debug.Log("Ate food");
+
+        //task 5
+        await RunAll();
+        Debug.Log("The end");
     }
 
-    // Update is called once per frame
-    void Update()
+    //task 1
+    private async void CookMeals()
     {
-        
+        Debug.Log("Making food");
+        await Task.Delay(5000);
+        Debug.Log("The food is burnt...");
     }
+
+    //task 2
+    private async Task ItalianFood()
+    {
+        Debug.Log("Pizza");
+        await Task.Delay(2000);
+        Debug.Log("Pasta");
+
+        await MetAHomeless();
+    }
+
+    //task 3
+
+    private async Task MetAHomeless()
+    {
+        Debug.Log("Met with a homeless guy");
+        await Task.Delay(1500);
+        Debug.Log("Gave my food to the homeless guy");
+
+        CookMeals();
+    }
+
+    //task 5
+    private async Task RunAll()
+    {
+        await Task.WhenAll(
+            ItalianFood(),
+            MetAHomeless()
+            );
+    }
+
 }
